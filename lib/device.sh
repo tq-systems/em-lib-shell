0dfaeba8cfa2558c278100017d0fcccd2a2bc54c
Christoph Krutz <christoph.krutz@tq-group.com>
2025-09-01 12:03:41 +0200

    Add lib/device.sh for specific device information of the Energy Manager
    Signed-off-by: Christoph Krutz <christoph.krutz@tq-group.com>

---
+++ lib/device.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# SPDX-License-Identifier: LicenseRef-TQSPSLA-1.0.3
+#
+# More license information can be found in the root folder.
+# This file is part of the shell library.
+#
+# Copyright (c) 2025 TQ-Systems GmbH <license@tq-group.com>, D-82229 Seefeld, Germany. All rights reserved.
+# Author: Christoph Krutz
+
+# shellcheck source=log.sh
+. "$TQEM_SHELL_LIB_DIR/log.sh"
+
+print_device_info() {
+	local command="$1"
+	local device="$2"
+
+	# tqs,energymanager310 needed for upgrades from old kernels
+	local bootloaders_em310='
+	tq_em310_256m=u-boot.sb-em310
+	tqs_energymanager310_256m=u-boot.sb-em310
+'
+	local bootloaders_em4xx='
+	tq_em4xx_512m=bootloader-em4xx-512m.bin
+	tq_em4xx_1024m=bootloader-em4xx-1g.bin
+'
+
+	local bootloaders_em_cb30='
+	tq_am625-em-cb30_512m=bootloader-em-cb30-512m.bin
+	tq_am625-em-cb30_1024m=bootloader-em-cb30-1g.bin
+	tq_am625-em-cb30_2048m=bootloader-em-cb30-2g.bin
+'
+
+	local device_type device_subtype machine device_arch product_id bootloaders
+
+	case "$device" in
+	em310|hw0100)
+		device_type='hw0100'
+		machine='em310'
+		device_arch='armv5e'
+		product_id='{"tq,em310": 18514}'
+		bootloaders="$bootloaders_em310"
+		;;
+	em4xx|hw0200)
+		device_type='hw0200'
+		machine='em-aarch64'
+		device_arch='aarch64'
+		product_id='{"tq,em4xx": 18530}'
+		bootloaders="$bootloaders_em4xx"
+		;;
+	em-cb30|hw0210)
+		device_type='hw0210'
+		machine='em-aarch64'
+		device_arch='aarch64'
+		product_id='{"tq,am625-em-cb30": 18546}'
+		bootloaders="$bootloaders_em_cb30"
+		;;
+	em-aarch64|hw02xx)
+		device_type='hw02xx'
+		device_subtype='{"tq,em4xx": "hw0200", "tq,am625-em-cb30": "hw0210"}'
+		machine='em-aarch64'
+		device_arch='aarch64'
+		product_id='{"tq,em4xx": 18530, "tq,am625-em-cb30": 18546}'
+		bootloaders="$bootloaders_em4xx $bootloaders_em_cb30"
+		;;
+	*)
+		tqem_log_error_and_exit "Unknown device: $device"
+		;;
+	esac
+
+	case "$command" in
+	type)
+		echo "$device_type"
+		;;
+	subtype)
+		echo "$device_subtype"
+		;;
+	machine)
+		echo "$machine"
+		;;
+	arch)
+		echo "$device_arch"
+		;;
+	product-id)
+		echo "$product_id"
+		;;
+	bootloaders)
+		# pipe through xargs for consistent formatting
+		echo "$bootloaders" | xargs
+		;;
+	*)
+		tqem_log_error_and_exit "Unknown command: $command"
+		;;
+	esac
+}
